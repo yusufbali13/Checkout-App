f4c0023f60234a95e314cd08e6b63765181797a5
Yusuf <baliyusuf675@gmail.com>
2023-06-13 17:26:38 +0300

    style

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-//********SELECTOR */
+// ******selectors
 
 const main = document.getElementById("products-preview");
 const nav = document.querySelector("nav");
 
-const amountofProducts = document.getElementById("span");
+const amountOfProducts = document.querySelector(".amount-of-product");
 
 const selectedProducts = document.querySelector(".main__sum-price");
 
@@ -13,7 +13,81 @@ const shipping = document
 const tax = document.getElementById("cart-tax").querySelector(".dollar");
 const total = document.getElementById("cart-total").querySelector(".dollar");
 
-//********EVENTS */
+// ******** events
+
 window.addEventListener("load", (e) => {
-  selectedProducts;
+  selectedProducts.innerHTML = updateAmount();
+  finishOreder();
 });
+
+main.addEventListener("click", (e) => {
+  if (e.target.classList.contains("fa-minus")) {
+    if (e.target.nextElementSibling.innerText > 1) {
+      e.target.nextElementSibling.innerText--;
+
+      e.target
+        .closest(".main__product")
+        .querySelector(".main__product-line-price").innerText = (
+        e.target.nextElementSibling.innerText *
+        e.target.closest(".main__product-info").querySelector(".dollar")
+          .innerText
+      ).toFixed(2);
+    }
+  } else if (e.target.classList.contains("fa-plus")) {
+    e.target.previousElementSibling.innerText++;
+
+    e.target
+      .closest(".main__product")
+      .querySelector(".main__product-line-price").innerText = (
+      e.target.previousElementSibling.innerText *
+      e.target.closest(".main__product-info").querySelector(".dollar").innerText
+    ).toFixed(2);
+  } else if (e.target.classList.contains("fa-trash-can")) {
+    if (confirm("are you sure???") == true) {
+      e.target.closest(".main__product").remove();
+
+      amountOfProducts.innerText--;
+    } else {
+    }
+  }
+  selectedProducts.innerHTML = updateAmount();
+  finishOreder();
+});
+
+nav.addEventListener("click", (e) => {
+  if (e.target.classList.contains("fa-trash-can")) {
+    document.querySelectorAll(".main__product");
+    for (let i of document.querySelectorAll(".main__product")) {
+      i.remove();
+      selectedProducts.innerText = updateAmount();
+      finishOreder();
+      amountOfProducts.innerText--;
+    }
+  }
+});
+// function subTotal(position) {
+//     e.target.closest(".main__product").querySelector(".main__product-line-price").innerText = (e.target.{position}.innerText * e.target.closest(".main__product-info").querySelector(".dollar").innerText).toFixed(2)
+// }
+
+function updateAmount() {
+  let totalAmount = document.getElementsByClassName("main__product-line-price");
+
+  let sum = 0;
+  for (let i of totalAmount) {
+    sum += +i.innerText;
+  }
+  return sum.toFixed(2);
+}
+
+function finishOreder() {
+  Number(selectedProducts.innerText) >= 3000 ||
+  Number(selectedProducts.innerText) == 0
+    ? (shipping.innerText = "0")
+    : (shipping.innerText = "25.99");
+  tax.innerText = (Number(selectedProducts.innerText) * 0.18).toFixed(2);
+  total.innerText = (
+    Number(shipping.innerText) +
+    Number(tax.innerText) +
+    Number(selectedProducts.innerText)
+  ).toFixed(2);
+}
